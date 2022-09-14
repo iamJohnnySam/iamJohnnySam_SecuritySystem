7bdbb4fa159bc429f5186f0c69fe2bca4e136cef
John Samarasinghe <john.samarasinghe@outlook.com>
2022-09-14 16:34:15 +0530

    Updates

--- CCTVCamera/CCTVclassify.py
+++ CCTVCamera/CCTVclassify.py
@@ -1,17 +1,15 @@
 import tensorflow as tf
 
 class CCTVclassify:
-
-    
-
     def __init__ (self, tfLocation):
         self.model = tf.lite.Interpreter(model_path=tfLocation)
         self.model.allocate_tensors()
-        self.input_details = self.model.get_input_details()
+        #self.input_details = self.model.get_input_details()
         self.output_details = self.model.get_output_details()
 
-    def classify (img):
+    def classify (self, img):
         self.model.set_tensor(self.input_details[0]['index'], img)
-        modelA01.invoke()
-        output_data = modelA01.get_tensor(output_details1[0]['index'])
+        self.model.invoke()
+        output_data = self.model.get_tensor(self.output_details[0]['index'])
         output = output_data[0][0]
+        return output
--- CCTVCamera/emailViewer.py
+++ CCTVCamera/emailViewer.py
@@ -1,135 +1,144 @@
 import imaplib
+import smtplib, ssl
 import email
-import time
+import os
 import numpy as np
 import shutil
 import logging
 from ..Global.globalVariables import *
-
-
-output = 0.000001
-foldersize = 0
+import CCTVclassify
+from os.path import basename
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.utils import formatdate
 
 from PIL import Image, ImageOps
 
-def outlookConnect ():
-    outlook = imaplib.IMAP4_SSL('outlook.office365.com', 993)
-    outlook.login(em, pw)
-    outlook.select(mailbox = 'Security', readonly=False)
+class getEmails():
+    att_path = "No attachment found."
+    download_folder="/home/pi/CCTV/Images"
+    save_folder="/home/pi/CCTV/Detected"
 
-def outlookGetCCTV (mod1, mod2):
-    (result, messages) = outlook.search(None, 'UnSeen')
-    if result == "OK":
-        for message in messages[0].split():
-            try: 
-                ret, data = outlook.fetch(message,'(RFC822)')
-            except:
-                logging.info("No new emails to read.")
-                outlook.connection.close()
+    foldersize = 0
 
-            msg = email.message_from_bytes(data[0][1])
-            Date = msg['Date']
-            Date = Date.replace(" +0530", "")
+    em = "sfgsdfgds"
+    pw = "sdssdf"
+
+    def __init__(self, folder):
+        self.outlook = imaplib.IMAP4_SSL('outlook.office365.com', 993)
+        self.outlook.login(self.em, self.pw)
+        self.outlook.select(mailbox = folder, readonly=False)
+
+        self.modelA01 = CCTVclassify("/home/pi/CCTV/model1.tflite")
+        self.modelA02 = CCTVclassify("/home/pi/CCTV/model1.tflite")
+
+    def close_connection(self):
+        self.connection.close()
+
+    def runCCTV(self):
+        (result, messages) = self.outlook.search(None, 'UnSeen')
+
+        if result == "OK":
+            for message in messages[0].split():
+                try: 
+                    ret, data = self.outlook.fetch(message,'(RFC822)')
+                except:
+                    logging.info("No new emails to read.")
+                    self.close_connection()
+
+                msg = email.message_from_bytes(data[0][1])
+                Date = msg['Date']
+                Date = Date.replace(" +0530", "")
         
-            att_path = "No attachment found."
-            for part in msg.walk():
-                if part.get_content_maintype() == 'multipart':
-                    continue
-                if part.get('Content-Disposition') is None:
-                    continue
+                for part in msg.walk():
+                    if part.get_content_maintype() == 'multipart':
+                        continue
+                    if part.get('Content-Disposition') is None:
+                        continue
+                    self.assessAttachment(part, Date)
 
-                filename = part.get_filename()
-                saveas = Date+" "+filename
-                att_path = os.path.join(download_folder, saveas)
+                ret, data = self.outlook.store(message, '+FLAGS','\\Deleted')
+
+    def assessAttachment(self, part, Date):
+        filename = part.get_filename()
+        saveas = Date+" "+filename
+        self.att_path = os.path.join(self.download_folder, saveas)
             
-                image = part.get_payload(decode=True)
-                fp = open(att_path, 'wb')
-                fp.write(image)
-                fp.close()
+        image = part.get_payload(decode=True)
+        fp = open(self.att_path, 'wb')
+        fp.write(image)
+        fp.close()
             
-                # Prepare Image
-                img = Image.open(att_path)
-                img = ImageOps.grayscale(img)
-                img = np.asarray(img,dtype="float32")/255
-                img = np.expand_dims(img, axis=0)
+        # Prepare Image
+        img = Image.open(self.att_path)
+        img = ImageOps.grayscale(img)
+        img = np.asarray(img,dtype="float32")/255
+        img = np.expand_dims(img, axis=0)
             
-                Sus = False
-                # ML CODE
-                if "A01" in filename:
-                    
-                    if (output > 0.95):
-                        Sus = True
+        Sus = False
+        # ML CODE
+        output = 0.000001
+
+        if "A01" in filename:
+            output = self.modelA01.classify (img)
+            if (output > 0.95):
+                Sus = True
             
-                if "A02" in filename:
-                    modelA02.set_tensor(input_details2[0]['index'], img)
-                    modelA02.invoke()
-                    output_data = modelA02.get_tensor(output_details2[0]['index'])
-                    output = output_data[0][0]
-                    if (output > 0.95):
-                        Sus = True
+        if "A02" in filename:
+            output = self.modelA02.classify (img)
+            if (output > 0.95):
+                Sus = True
             
-                if not Sus:
-                    os.remove(att_path)
+        if not Sus:
+            os.remove(self.att_path)
                         
-                if Sus:
-                    foldersize = foldersize + os.path.getsize(att_path)
-                    print(filename, output, "{:,}".format(foldersize))
+        if Sus:
+            foldersize = foldersize + os.path.getsize(self.att_path)
+            print(filename, output, "{:,}".format(foldersize))
 
-            response, data = outlook.store(message, '+FLAGS','\\Deleted')
 
-outlookDisconnect():
-    outlook.close()
+    def sendSus(self):
+        send_to = ['john.samarasinghe@outlook.com', 'gayathri_karunaratne@yahoo.com']
 
-send_to = ['john.samarasinghe@outlook.com', 'gayathri_karunaratne@yahoo.com']
-
-if (foldersize > 0):
-    print("Sending email")
-    import smtplib, ssl
-    from os.path import basename
-    from email import encoders
-    from email.mime.base import MIMEBase
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-    from email.mime.application import MIMEApplication
-    from email.utils import COMMASPACE, formatdate
+        if (self.foldersize > 0):
+            send_from = self.em
+            body = """\
+                <html>
+                    <body>
+                        <p>Hi mau mau and mau,<br>
+                           We detected some activity outside the gate in the last 1 hour.<br>
+                        </p>
+                    </body>
+                </html>
+            """
     
-    send_from = em
-    body = """\
-        <html>
-            <body>
-                <p>Hi mau mau and mau,<br>
-                   We detected some activity outside the gate in the last 1 hour.<br>
-                </p>
-            </body>
-        </html>
-    """
+            msg = MIMEMultipart()
+            msg['From'] = send_from
+            msg['To'] = ", ".join(send_to)
+            msg['Date'] = formatdate(localtime=True)
+            msg['Subject'] = "Suspicious Activity at 110/2"
     
-    msg = MIMEMultipart()
-    msg['From'] = send_from
-    msg['To'] = ", ".join(send_to)
-    msg['Date'] = formatdate(localtime=True)
-    msg['Subject'] = "Suspicious Activity at 110/2"
+            msg.attach(MIMEText(body, "html"))
     
-    msg.attach(MIMEText(body, "html"))
-    
-    size = 0
-    for f in os.listdir(download_folder):
-        with open(os.path.join(download_folder, f), "rb") as fil:
-            part = MIMEApplication(fil.read(),Name=basename(f))
-        # After the file is closed
-        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
-        msg.attach(part)
-        size = size + os.path.getsize(os.path.join(download_folder, f))
-        foldersize = foldersize - os.path.getsize(os.path.join(download_folder, f))
-        # os.remove(os.path.join(download_folder, f))
-        shutil.move(os.path.join(download_folder, f), os.path.join(save_folder, f))
-        if (size > (4.9*1024*1024)):
-            break
+            size = 0
+            for f in os.listdir(self.download_folder):
+                with open(os.path.join(self.download_folder, f), "rb") as fil:
+                    part = MIMEApplication(fil.read(),Name=basename(f))
+                # After the file is closed
+                part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+                msg.attach(part)
+                size = size + os.path.getsize(os.path.join(self.download_folder, f))
+                foldersize = foldersize - os.path.getsize(os.path.join(self.download_folder, f))
+                # os.remove(os.path.join(self.download_folder, f))
+                shutil.move(os.path.join(self.download_folder, f), os.path.join(self.save_folder, f))
+                if (size > (4.9*1024*1024)):
+                    break
 
 
-    context = ssl.create_default_context()
-    server = smtplib.SMTP("smtp.office365.com", 587)
-    server.starttls(context=context)
-    server.login(em, pw)
-    server.sendmail(send_from, send_to, msg.as_string())
-    server.close()
+            context = ssl.create_default_context()
+            server = smtplib.SMTP("smtp.office365.com", 587)
+            server.starttls(context=context)
+            server.login(self.em, self.pw)
+            server.sendmail(send_from, send_to, msg.as_string())
+            server.close()
